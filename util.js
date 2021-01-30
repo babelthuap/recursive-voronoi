28ae6b4a940b4b07697f45cff6d8ba7f54234e4d
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2021-01-29 22:55:04 -0600

    allow non-integer metrics

--- util.js
+++ util.js
@@ -2,24 +2,27 @@
 export const rand = (n) => Math.floor(Math.random() * n);
 
 /**
+ * Returns the sqaure of the Euclidean distance between two points in R^2.
+ */
+const euclideanDist = (x1, y1, x2, y2) => {
+  const dx = x1 - x2;
+  const dy = y1 - y2;
+  return dx * dx + dy * dy;
+};
+
+/**
  * Returns the un-rooted distance between two points in R^2 according to the
  * configured metric. Sufficient for comparing distances.
  */
 export const distance = (() => {
-  const possibleMetics = {1: 1, 2: 2, 3: 3, 4: 4};
-  const metric =
-      possibleMetics[new URLSearchParams(location.search).get('metric')] || 2;
+  const metric = Number(new URLSearchParams(location.search).get('metric'));
   switch (metric) {
     case 1:
       // taxicab distance
       return (x1, y1, x2, y2) => Math.abs(x1 - x2) + Math.abs(y1 - y2);
     case 2:
       // euclidean distance
-      return (x1, y1, x2, y2) => {
-        const dx = x1 - x2;
-        const dy = y1 - y2;
-        return dx * dx + dy * dy;
-      };
+      return euclideanDist;
     case 3:
       // cubic distance
       return (x1, y1, x2, y2) => {
@@ -38,5 +41,17 @@ export const distance = (() => {
         dy *= dy;
         return dx + dy;
       };
+    default:
+      if (metric > 1) {
+        // generalized distance: dx^m + dy^m
+        return (x1, y1, x2, y2) => {
+          const dx = Math.abs(x1 - x2);
+          const dy = Math.abs(y1 - y2);
+          return Math.pow(dx, metric) + Math.pow(dy, metric);
+        };
+      } else {
+        // fall back to euclidean distance
+        return euclideanDist;
+      }
   }
 })();
