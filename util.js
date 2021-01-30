03faa54a005242be9815f5d38d09875f730b6ce1
Nicholas Neumann-Chun <babelthuap@users.noreply.github.com>
2021-01-30 14:20:51 -0600

    enforce metric >= 1 to ensure convexity

--- util.js
+++ util.js
@@ -42,7 +42,7 @@ export const distance = (() => {
         return dx + dy;
       };
     default:
-      if (metric > 0) {
+      if (metric > 1) {
         // generalized distance: dx^m + dy^m
         return (x1, y1, x2, y2) => {
           const dx = Math.abs(x1 - x2);
